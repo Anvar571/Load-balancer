e754496e6242d2eebd2f953eec2ac842a52d3520
Anvar571 <abdurashidovanvar52@gmail.com>
2025-04-15 17:03:52 +0500

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+export default {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: "airbnb-base",
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: "latest",
+    },
+    rules: {
+      indent: ["error", 2],
+    },
+  };
+  
